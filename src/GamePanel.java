ff27d1bc0fe4ed9a447a23e961fc2064886444d1
Ivor Metcalf <ivormetcalf@gmail.com>
2015-05-15 22:46:24 +1000

    Created a game framework
    Made GamePanel, MainGame, Vec2, GameObject Classes

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,30 +1,58 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
 import javax.swing.JPanel;
 
 
 public class GamePanel extends JPanel implements ActionListener {
-
-	
-	
 	
 	private static final long serialVersionUID = 1L;
 	
 	public Dimension getPreferredSize() {
-        return new Dimension(640,480);
+        return new Dimension(600,600);
     }
 	
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);       
+        super.paintComponent(g);
+        
+        Graphics2D g2d = (Graphics2D) g;        
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+        		RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        if(windowResized)
+        {
+        	for(GameObject gameObj : gameObjects)
+        	{
+        		gameObj.OnResize(getSize());
+        	}
+        }
+        
+        for(GameObject gameObj : gameObjects)
+        {
+        	gameObj.Draw(g2d);
+        }
     }
+    
+    
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void SetGameObjectList(LinkedList<GameObject> GameObjects)
+	{
+		gameObjects = GameObjects;
+	}
+	
+	boolean windowResized = false;
+	
+	LinkedList<GameObject> gameObjects;
 
 }
